e50643f41381cf9b569cfb3329945904630a736c
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2023-01-03 18:17:17 +0900

    box

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod lesson_array_slice_1;
 mod lesson_borrowing_1;
 mod lesson_borrowing_2;
 mod lesson_borrowing_3;
+mod lesson_box_1;
 mod lesson_closures_1;
 mod lesson_closures_2;
 mod lesson_life_time_01;
